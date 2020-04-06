00f10b40125d6a2d9eabfb9ca511949f2ab442da
Veaceslav <aramexfrontend@mail.ru>
2020-04-06 10:18:11 +0300

    observables

--- src/app/login-user/login-user.component.ts
+++ src/app/login-user/login-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup ,Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { PasswordValidator } from './../passwordValodator';
+import {filter, debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-user',
@@ -13,18 +14,31 @@ export class LoginUserComponent implements OnInit {
 
   title2 = 'Implementing Custom Validation';
 
+  title3 = 'Observable';
+
   /* form = new FormGroup({
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
   }); */
 
+  searchControl = new FormControl();
+
   form: FormGroup;
+
   constructor(fb: FormBuilder) {
       this.form = fb.group({
         username: ['', Validators.required],
         password: ['', Validators.compose([Validators.required,
            PasswordValidator.cannotContainSpace])]
       });
+
+      this.searchControl.valueChanges.pipe(
+        filter(text => text.length >= 3),
+        debounceTime(4000),
+        distinctUntilChanged()
+      ).subscribe(value => {console.log(value)});
+        
+
 
    }
 
@@ -33,5 +47,7 @@ export class LoginUserComponent implements OnInit {
   login() {
     console.log(this.form.value);
   }
+
+
 
 }
